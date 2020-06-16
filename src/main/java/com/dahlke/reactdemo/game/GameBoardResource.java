00f44cc14c2757ff6293ab13b8ccacb14461d1f6
Neil Dahlke <neil@dahlke.io>
2020-06-16 13:48:42 -0700

    Works now with passing parameters in.

--- src/main/java/com/dahlke/reactdemo/game/GameBoardResource.java
+++ src/main/java/com/dahlke/reactdemo/game/GameBoardResource.java
@@ -15,6 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,6 +28,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 
 
 /**
@@ -42,10 +44,15 @@ public class GameBoardResource {
 	private int port = 3000;
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
-	@RequestMapping(path = "/game", method = GET)
+	@RequestMapping(path = "/game", method = POST)
+	@ResponseBody
 	//consumes = "application/json", produces = "application/json"
-	// public GameBoard game() throws IOException {
-	public String game() throws IOException {
+	public String game(
+		@RequestParam(name="name", defaultValue="test", required=false) String name,
+		@RequestParam(name="rows", defaultValue="10", required=false) Integer rows,
+		@RequestParam(name="cols", defaultValue="10", required=false) Integer cols,
+		@RequestParam(name="mines", defaultValue="20", required=false) Integer mines
+	) throws IOException {
 		RestTemplate restTemplate = new RestTemplate();
 		String gameUrl = "http://localhost:3000/game";
 
@@ -53,41 +60,20 @@ public class GameBoardResource {
 		headers.setContentType(APPLICATION_JSON);
 
 		JSONObject gameRequestJson = new JSONObject();
-		gameRequestJson.put("name", "test");
-		gameRequestJson.put("rows", 10);
-		gameRequestJson.put("cols", 10);
-		gameRequestJson.put("mines", 20);
+		gameRequestJson.put("name", name);
+		gameRequestJson.put("rows", rows);
+		gameRequestJson.put("cols", cols);
+		gameRequestJson.put("mines", mines);
 
+		// curl -i -X POST '127.0.0.1:8080/api/game' -d '{"name": "teste", "rows": 10, "cols": 8, "mines": 20}'
+		// curl -i -X POST '127.0.0.1:8080/api/game' -d name=test -d rows=10 -d cols=10 -d mines=20
+
+		System.out.println(gameRequestJson.toString());
 		HttpEntity<String> request = new HttpEntity<String>(gameRequestJson.toString(), headers);
-
-		// GameBoard gameBoard = restTemplate.postForObject(gameUrl, request, GameBoard.class);
-		// System.out.println(gameBoard);
-
-		// TODO: will probably have to parse out the results portion of this first. 
 		String gameBoardAsString = restTemplate.postForObject(gameUrl, request, String.class);
 		JsonNode root = objectMapper.readTree(gameBoardAsString);
 		JsonNode result = root.path("result");
 		return result.toString();
 	}
 
-/*
-	@RequestMapping("/**")
-	@ResponseBody
-	public String mirrorRest(@RequestBody String body, HttpMethod method, HttpServletRequest request) throws URISyntaxException
-	{
-		System.out.println(server);
-		System.out.println(port);
-		System.out.println(request.getRequestURI());
-		RestTemplate restTemplate = new RestTemplate();
-		URI uri = new URI("http", null, server, port, request.getRequestURI(), request.getQueryString(), null);
-
-		System.out.println(uri);
-
-
-		// curl -i -X POST '127.0.0.1:3000/game' -d '{"name": "teste", "rows": 10, "cols": 8, "mines": 20}'
-		// curl -i -X POST '127.0.0.1:3001/api/game' -d '{"name": "teste", "rows": 10, "cols": 8, "mines": 20}'
-
-		return responseEntity.getBody();
-	}
-	*/
 }
